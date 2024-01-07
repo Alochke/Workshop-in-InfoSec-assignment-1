d0328085a70bcafe55835ff75f83d5ae3973574d
Alochke <sageuthanasia@gmail.com>
2024-01-07 16:36:43 +0200

    Fixing hw1secws.c compilation error.

--- hw1secws.c
+++ hw1secws.c
@@ -26,7 +26,7 @@
 #define ERR_CHECK(condition, extra_code, function, errno_value){\
     if(condition){\
         extra_code;\
-        printk(KERN_ERR funtion " failed.");\
+        printk(KERN_ERR " failed.");\
         return errno_value;\
     }\
 }
