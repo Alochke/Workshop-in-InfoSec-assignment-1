3fdda6661028cb5006d9d48b8618aeaaa279e880
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-18 21:59:19 +0200

    Added the TRUE macro to sysfs.h, it's necessary.

--- sysfs.h
+++ sysfs.h
@@ -11,6 +11,7 @@
 #define NAME_OF_DEVICE "FW_Device"
 #define NAME_OF_CLASS "FW_class"
 #define SUCCESS 0
+#define TRUE 1
 #define NUMBER_OF_BYTES_TRANSFERED 2 * sizeof(unsigned int) /* Number of bytes transferd inside the display function in sysfs.c from kernel space to user-space. */
 #define SYSFS_MINOR 0 // To use with MKDEV to generate a number for a sysfs device.
 
