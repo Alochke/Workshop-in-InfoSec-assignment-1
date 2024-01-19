a1a36c83af45b033faf98f88b55a1def353d18d6
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-19 23:04:41 +0200

    Maybe done with the userspace program.

--- fw_interface.c
+++ fw_interface.c
@@ -1,16 +1,18 @@
 #include <string.h> // For strcmp.
 #include <stdio.h>  // For reading from and writing to files and for printing.
 
+
 #define ATTRIBUTE_PATH "/sys/class/fw_class/fw_class_fw_device/sysfs_att"
 #define CORRECT_INPUT "0" // Only correct non-empty input.
-#define ERROR_MSG "Wrong input, you've entered."
-#define MAX_INPUTS 1 // Maximal number of parameters to the program.
+#define ERROR_MSG "Wrong input, you've entered.\n"
+#define MAX_INPUTS 2 // Maximal number of parameters to the program.
 #define EQ 0 // The value strcmp returns when the compared strings are equel.
 #define SHOW_TRANSFER 2 * sizeof(unsigned int) // The amount data transfered from the kernel module to a buffer by reading from ATTRIBUTE_PATH.
 #define ERROR -1 // Will be returned in case of an error (which could happen only if wrong paarameters were given.)
 #define SUCCES 1 // Will be returned in case the program was executed successfuly.
+#define CLI_PARAM_IDX 1 // The index of the only parameter, given to the command line when one was given in argv.
 
-int main( int argc, char *argv[] )
+int main( int argc, char* argv[] )
 {
     FILE* fptr;                 // Will be used to interact with the sysfs attribute.
     unsigned int accepted;      // The number of accepted packetes.
@@ -18,7 +20,7 @@ int main( int argc, char *argv[] )
     char buf[SHOW_TRANSFER];   // Will be used to store the data transfered from the module in case no arguments were given to the program.
 
     // Checking input.
-    if(argc > MAX_INPUTS || ((argc == MAX_INPUTS) && (strcmp(CORRECT_INPUT, argv[MAX_INPUTS]) != EQ)))
+    if(argc > MAX_INPUTS || ((argc == MAX_INPUTS) && (strcmp(CORRECT_INPUT, argv[CLI_PARAM_IDX]) != EQ)))
     {
         printf(ERROR_MSG);
         return ERROR;
@@ -35,7 +37,7 @@ int main( int argc, char *argv[] )
         printf("Firewall Packets Summary:\n");
         printf("Number of accepted packets: %d\n", accepted);
         printf("Number of dropped packets: %d \n", dropped);
-        printf("Total number of packets: 278 %d \n", accepted + dropped);
+        printf("Total number of packets: %d \n", accepted + dropped);
     }
 
     fclose(fptr);
