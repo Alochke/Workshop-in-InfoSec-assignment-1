fbbd9565a4b21ac22aa9c39af741eb89c006df3f
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-20 17:27:38 +0200

    Uploading for last testing.

--- fw_interface.c
+++ fw_interface.c
@@ -5,18 +5,24 @@
 
 
 #define ATTRIBUTE_PATH "/sys/class/fw_class/fw_class_fw_device/sysfs_att"
-#define CORRECT_INPUT "0"                                       // Only correct non-empty input.
-#define INPUT_ERR "Wrong input, you've entered.\n"              // The error message in case of wrong input.
-#define PERMISSION_ERR "As a sudo, you must run the program.\n" // The error message in case the program wasn't run with the right permissions.
-#define MAX_INPUTS 2                                            // Maximal number of parameters to the program, including program name
-#define EQ 0                                                    // The value strcmp returns when the compared strings are equel.
-#define SHOW_TRANSFER 2 * sizeof(unsigned int)                  // The amount data transfered from the kernel module to a buffer by reading from ATTRIBUTE_PATH.
-#define ERROR -1                                                // Will be returned in case of an error (which could happen only if wrong paarameters were given.)
-#define SUCCES 1                                                // Will be returned in case the program was executed successfuly.
-#define CLI_PARAM_IDX 1                                         // The index of the only parameter, given to the command line when one was given in argv.
-#define MINIMAL_TRANSFER 1                                      // The minimal amount of bytes it's guaranteed that read will transfer.
+#define CORRECT_INPUT "0" // Only correct non-empty input.
+#define INPUT_ERR "Wrong input, you've entered.\n" // The error message in case of wrong input.
+#define OPEN_ERR "As a sudo, you didn't run the program, or the module, you didn't install.\n" // The error message in case the program wasn't run with the right permissions or the module wasn't installed before running the program.
+#define MAX_ARGC 2 // Maximal number value of argc.
+#define EQ 0 // The value strcmp returns when the compared strings are equel.
+#define SHOW_TRANSFER 2 * sizeof(unsigned int) // The amount of data that should be transferred from the kernel module in case no arguments were given to the program.
+#define ERROR -1 // Will be returned in case of an error.
+#define SUCCESS 0 // Will be returned in case the program was executed successfuly.
+#define PARAM_IDX 1 // The index in argv of the only parameter given to the command line when one was given.
 
-int main( int argc, char* argv[] )
+// The next 4 lines are the output message lines in case no arguments were given to the program.
+
+#define OUT_LINE1 "Firewall Packets Summary: \n"
+#define OUT_LINE2 "Number of accepted packets: %u \n", accepted
+#define OUT_LINE3 "Number of dropped packets: %u \n", dropped
+#define OUT_LINE4 "Total number of packets: %u \n", accepted + dropped
+
+int main(int argc, char* argv[])
 {
     int fd;                     // A file descriptor that will be used to interact with the sysfs attribute.
     unsigned int accepted;      // The number of accepted packetes.
@@ -24,7 +30,7 @@ int main( int argc, char* argv[] )
     char buf[SHOW_TRANSFER];    // Will be used to store the data transfered from the module in case no arguments were given to the program.
 
     // Checking input.
-    if(argc > MAX_INPUTS || ((argc == MAX_INPUTS) && (strcmp(CORRECT_INPUT, argv[CLI_PARAM_IDX]) != EQ)))
+    if(argc > MAX_ARGC || ((argc == MAX_ARGC) && (strcmp(CORRECT_INPUT, argv[PARAM_IDX]) != EQ)))
     {
         printf(INPUT_ERR);
         return ERROR;
@@ -33,26 +39,26 @@ int main( int argc, char* argv[] )
     fd = open(ATTRIBUTE_PATH, O_RDWR);
     if(fd == ERROR)
     {
-        printf(PERMISSION_ERR);
+        printf(OPEN_ERR);
         return ERROR;
     }
 
-    if(argc == MAX_INPUTS)
+    if(argc == MAX_ARGC)
         write(fd, NULL, 0);
     else{
-        int to_transfer = SHOW_TRANSFER;
         int transfered = 0;
-        while(to_transfer != transfered)
+        while(SHOW_TRANSFER != transfered)
         {
-            transfered += read(fd, buf + transfered, MINIMAL_TRANSFER);
+            transfered += read(fd, buf + transfered, SHOW_TRANSFER - transfered);
         }
         accepted = (unsigned int) *buf;
         dropped = (unsigned int) *(buf + sizeof(unsigned int));
-        printf("Firewall Packets Summary:\n");
-        printf("Number of accepted packets: %d\n", accepted);
-        printf("Number of dropped packets: %d \n", dropped);
-        printf("Total number of packets: %d \n", accepted + dropped);
+        printf(OUT_LINE1);
+        printf(OUT_LINE2);
+        printf(OUT_LINE3);
+        printf(OUT_LINE4);
     }
 
     close(fd);
+    return SUCCESS;
 }
